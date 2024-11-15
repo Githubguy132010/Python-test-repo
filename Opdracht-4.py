6c763f913a54f0271b20e98288f95d47db01b32b
Thomas Brugman <thomas.brugman.teb3@gmail.com>
2024-11-15 21:09:10 +0100

    Perfecties

--- Opdracht-4.py
+++ Opdracht-4.py
@@ -1,10 +1,10 @@
 correct_wachtwoord = "python123"
 
-wachtwoord = input ("Voer het wachtwoord in")
+wachtwoord = input (" Voer het wachtwoord in")
 
 while wachtwoord != correct_wachtwoord:
-    print("Verkeerd wachtwoord, probeer het opnieuw")
-    wachtwoord = input ("Voer het wachtwoord opnieuw in")
+    print(" Verkeerd wachtwoord, probeer het opnieuw")
+    wachtwoord = input (" Voer het wachtwoord opnieuw in")
 
 print ("Toegang verleend!")
 
